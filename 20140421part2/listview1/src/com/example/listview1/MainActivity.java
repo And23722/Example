8a1d5eaa5fcb33f5ce1fb50da15f400107dff518
And23722 <aszx15987@yahoo.com.tw>
2014-04-21 21:07:13 +0800

    use simpleadapter

--- 20140421part2/listview1/src/com/example/listview1/MainActivity.java
+++ 20140421part2/listview1/src/com/example/listview1/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.listview1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,68 +16,86 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-    	
-    	private ListView listView;
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+		private ListView listView;
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            
-            listView = (ListView) rootView.findViewById(R.id.listView1);
+		public PlaceholderFragment() {
+		}
 
-			String[] data = new String[] { "1", "2", "3", "4", "5", "6", "7",
-					"8", "9", "10", "11", "12" };
-			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-					getActivity(), android.R.layout.simple_list_item_1, data);
-			listView.setAdapter(adapter);
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+
+			listView = (ListView) rootView.findViewById(R.id.listView1);
+
+			String[] text = new String[] { "1", "2", "3", "4", "5", "6", "7",
+					"8", "9", "10" };
 			
-            return rootView;
-        }
-    }
+			int[] imageIds = new int[] { R.drawable.img1, R.drawable.img2,
+					R.drawable.img3, R.drawable.img4, R.drawable.img5,
+					R.drawable.img6, R.drawable.img7, R.drawable.img8,
+					R.drawable.img9, R.drawable.img10 };
+			
+			List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+			
+			for(int i = 0; i<text.length; i++){
+				Map<String, Object> item = new HashMap<String, Object>();
+				 				item.put("text", text[i]);
+				 				item.put("image", imageIds[i]);
+				 				data.add(item);
+			}
+			
+			String[] from = new String[] { "text", "image" };
+			
+			int[] to = new int[] { R.id.textView1, R.id.imageView1 };
+			
+			SimpleAdapter adapter = new SimpleAdapter(getActivity(), data,
+					 					R.layout.list_item, from, to);
+			listView.setAdapter(adapter);
+			return rootView;
+		}
+	}
 
 }
