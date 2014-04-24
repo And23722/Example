b564e11830fcdae879bf7623e1cbaa010d19f8c4
And23722 <aszx15987@yahoo.com.tw>
2014-04-24 22:21:48 +0800

    update

--- 20140424part1/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140424part1/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -5,36 +5,36 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.TextView;
 
 @SuppressLint("NewApi")
 public class MessageActivity extends Activity {
-	
+
 	private TextView textView;
 	private static final String FILE_NAME = "text.txt";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_message);
 		textView = (TextView) findViewById(R.id.textView1);
 		String text = getIntent().getStringExtra("text");
-		textView.setText(text);
-		
+
 		writeFile(text);
-//		writeFile2(text);
+		writeFile2(text);
 		textView.setText(readFile());
 	}
-	
-	
-	private void writeFile(String text){
+
+	private void writeFile(String text) {
 		try {
 			text += "\n";
 
@@ -49,46 +49,47 @@ public class MessageActivity extends Activity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-//	private void writeFile2(String text) {
-//
-//		File docDir = Environment
-//				.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-//		if (docDir.exists() == false) {
-//			docDir.mkdirs();
-//		}
-//
-//		File file = new File(docDir, FILE_NAME);
-//
-//		try {
-//			text += "\n";
-//
-//			FileOutputStream fos = new FileOutputStream(file);
-//			fos.write(text.getBytes());
-//			fos.flush();
-//			fos.close();
-//
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-	
+
+	private void writeFile2(String text) {
+
+		File docDir = Environment
+				.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+
+		if (docDir.exists() == false) {
+			docDir.mkdirs();
+		}
+
+		File file = new File(docDir, FILE_NAME);
+//		Log.d("debug", file.getPath());
+
+		try {
+			text += "\n";
+
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(text.getBytes());
+			fos.flush();
+			fos.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private String readFile() {
-		try{
-			
+		try {
+
 			FileInputStream fis = openFileInput(FILE_NAME);
 			byte[] buffer = new byte[1024];
 			fis.read(buffer);
 			fis.close();
-			
+
 			return new String(buffer);
-			
-		}catch (FileNotFoundException e) {
+
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
