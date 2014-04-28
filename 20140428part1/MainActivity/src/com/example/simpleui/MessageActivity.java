7b720c327f1b2b3436b24f3ac45de9979c068632
And23722 <aszx15987@yahoo.com.tw>
2014-04-28 20:42:59 +0800

    add SaveCallback

--- 20140428part1/MainActivity/src/com/example/simpleui/MessageActivity.java
+++ 20140428part1/MainActivity/src/com/example/simpleui/MessageActivity.java
@@ -2,12 +2,16 @@ package com.example.simpleui;
 
 
 
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class MessageActivity extends Activity {
@@ -21,15 +25,38 @@ public class MessageActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_message);
+		boolean isChecked = getIntent().getBooleanExtra("checkBox", false); 
 		textView = (TextView) findViewById(R.id.textView1);
 		String text = getIntent().getStringExtra("text");
 
 		ParseObject testObject = new ParseObject("MessageObject");
 		testObject.put("text", text);
-		testObject.saveInBackground();
+		testObject.put("checkBox", isChecked);
+		testObject.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e){
+				
+				if (e == null) {
+					
+					Log.d("debug", "OK");
+					
+				}else {
+					
+					e.printStackTrace();
+				}
+				
+				ok();
+			}
+
+		});
+		
 		textView.setText(text);
 	}
 
-	
+	private void ok() {
+		
+		Toast.makeText(this, "done.", Toast.LENGTH_SHORT).show();
+		
+	}
 
 }
