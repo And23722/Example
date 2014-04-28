06ba839c21b6b4c1f7a79d45483f255c7a97ccd5
And23722 <aszx15987@yahoo.com.tw>
2014-04-28 22:07:20 +0800

    invoke camera

--- 20140428part2/takephoto/src/com/example/takephoto/MainActivity.java
+++ 20140428part2/takephoto/src/com/example/takephoto/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.takephoto;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.provider.MediaStore;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,7 +42,10 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_photo) {
+        	Intent intent = new Intent();
+			intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
