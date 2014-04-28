d009a60b39682d4d2619b8f83b18f26de7d41de7
And23722 <aszx15987@yahoo.com.tw>
2014-04-28 20:51:48 +0800

    loaddata

--- 20140428part1/MainActivity/src/com/example/simpleui/MessageActivity.java
+++ 20140428part1/MainActivity/src/com/example/simpleui/MessageActivity.java
@@ -2,8 +2,12 @@ package com.example.simpleui;
 
 
 
+import java.util.List;
+
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 import android.annotation.SuppressLint;
@@ -28,35 +32,47 @@ public class MessageActivity extends Activity {
 		boolean isChecked = getIntent().getBooleanExtra("checkBox", false); 
 		textView = (TextView) findViewById(R.id.textView1);
 		String text = getIntent().getStringExtra("text");
-
-		ParseObject testObject = new ParseObject("MessageObject");
-		testObject.put("text", text);
-		testObject.put("checkBox", isChecked);
-		testObject.saveInBackground(new SaveCallback() {
-			@Override
-			public void done(ParseException e){
-				
+		
+		saveData(text, isChecked);
+		loadData();
+	}
+	
+	private void loadData() {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("MessageObject");
+		query.findInBackground(new FindCallback<ParseObject>() {
+			public void done(List<ParseObject> messages, ParseException e) {
 				if (e == null) {
-					
-					Log.d("debug", "OK");
-					
-				}else {
-					
+					String content = "";
+					for (ParseObject message : messages) {
+						content += message.getString("text") + "\n";
+					}
+					textView.setText(content);
+				} else {
 					e.printStackTrace();
 				}
-				
-				ok();
 			}
-
-		});
-		
-		textView.setText(text);
+		});		
 	}
 
-	private void ok() {
-		
-		Toast.makeText(this, "done.", Toast.LENGTH_SHORT).show();
-		
-	}
+		private void saveData(String text, boolean isChecked) {
+			ParseObject testObject = new ParseObject("MessageObject");
+			testObject.put("text", text);
+			testObject.put("checkBox", isChecked);
+			testObject.saveInBackground(new SaveCallback() {
+				@Override
+				public void done(ParseException e) {
+					if (e == null) {
+						Log.d("debug", "OK");
+					} else {
+						e.printStackTrace();
+					}
+					ok();
+				}
+			});
+		}
+
+		private void ok() {
+			Toast.makeText(this, "done.", Toast.LENGTH_SHORT).show();
+		}
 
 }
