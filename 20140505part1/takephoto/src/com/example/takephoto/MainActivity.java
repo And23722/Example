c3fbe32887579634e56ee08ca913ec46cb72e55a
And23722 <aszx15987@yahoo.com.tw>
2014-05-05 20:18:57 +0800

    add imageview to display camera's photo

--- 20140505part1/takephoto/src/com/example/takephoto/MainActivity.java
+++ 20140505part1/takephoto/src/com/example/takephoto/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.takephoto;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -12,86 +13,92 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.os.Build;
 import android.provider.MediaStore;
 
+@SuppressLint("ValidFragment")
 public class MainActivity extends ActionBarActivity {
 
-    private static final int REQUEST_CODE_PHOTO = 55566;
+	private static final int REQUEST_CODE_PHOTO = 55566;
+
+	private ImageView imageView;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_photo) {
-        	Intent intent = new Intent();
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_photo) {
+			Intent intent = new Intent();
 			intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
 			startActivityForResult(intent, REQUEST_CODE_PHOTO);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    	// TODO Auto-generated method stub
-    	super.onActivityResult(requestCode, resultCode, intent);
-    	Log.d("debug","onActivityResult");
-    	
-    	if(requestCode == REQUEST_CODE_PHOTO){
-    		
-    		if(resultCode == RESULT_OK){
-    			Bitmap bitmap = intent.getParcelableExtra("data");
-    			Log.d("debug","OK");
-    			
-    		}else if(resultCode == RESULT_CANCELED){
-    			
-    			Log.d("debug","CANCELED");
-    			    			
-    		}else{
-    			
-    			
-    		}
-    	}
-    }
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, intent);
+		Log.d("debug", "onActivityResult");
 
-        public PlaceholderFragment() {
-        }
+		if (requestCode == REQUEST_CODE_PHOTO) {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+			if (resultCode == RESULT_OK) {
+				Bitmap bitmap = intent.getParcelableExtra("data");
+				imageView.setImageBitmap(bitmap);
+				Log.d("debug", "OK");
+
+			} else if (resultCode == RESULT_CANCELED) {
+
+				Log.d("debug", "CANCELED");
+
+			} else {
+
+			}
+		}
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public class PlaceholderFragment extends Fragment {
+
+		@SuppressLint("ValidFragment")
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			imageView = (ImageView) rootView.findViewById(R.id.imageView1);
+			return rootView;
+		}
+	}
 
 }
