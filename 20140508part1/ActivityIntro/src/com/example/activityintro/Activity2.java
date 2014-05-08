cfe859cad36473d9445704f1f718b994170f756b
And23722 <aszx15987@yahoo.com.tw>
2014-05-08 21:36:51 +0800

    more activity and interaction

---
+++ 20140508part1/ActivityIntro/src/com/example/activityintro/Activity2.java
@@ -0,0 +1,11 @@
+package com.example.activityintro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2 extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
